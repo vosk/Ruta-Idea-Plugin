ac0917021bfcb02ac26064cfde3223d0b4cad8b5
Manos Orfanoudakis <manos.orfanoudakis@gmail.com>
2019-04-06 19:03:28 +0300

    Fix: RutaDeclarationPsiNode NullPointer

--- src/main/java/vosk/ruta/psi/nodes/RutaDeclarationPsiNode.java
+++ src/main/java/vosk/ruta/psi/nodes/RutaDeclarationPsiNode.java
@@ -3,13 +3,10 @@ package vosk.ruta.psi.nodes;
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiNameIdentifierOwner;
 import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import vosk.ruta.psi.RutaFile;
-import vosk.ruta.psi.types.RutaType;
 
 public class RutaDeclarationPsiNode extends ASTWrapperPsiElement implements PsiNameIdentifierOwner {
 
@@ -17,15 +14,6 @@ public class RutaDeclarationPsiNode extends ASTWrapperPsiElement implements PsiN
     public RutaDeclarationPsiNode(@NotNull ASTNode node) {
         super(node);
     }
-
-
-    private void store(RutaType type){
-        PsiFile containingFile = this.getContainingFile();
-        if(containingFile instanceof RutaFile){
-            ((RutaFile) containingFile).getIndex().put(null);
-        }
-    }
-
 
     @Nullable
     @Override
@@ -45,7 +33,7 @@ public class RutaDeclarationPsiNode extends ASTWrapperPsiElement implements PsiN
     @Override
     public int getTextOffset() {
         PsiElement nameIdentifier = getNameIdentifier();
-        if (nameIdentifier == null) {
+        if (nameIdentifier != null) {
             return nameIdentifier.getTextOffset();
         } else {
             return this.getNode().getStartOffset();
